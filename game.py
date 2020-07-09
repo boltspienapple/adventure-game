6e3f8f1814a719d1864c32f88c8e6f216378758b
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-09 22:22:14 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,24 +1,29 @@
-import random
 import time
-print("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-time.sleep(2)
-print("Rumor has it that a gorgon is somewhere around here, and has been terrifying the nearby village.")
-time.sleep(2)
-print("In front of you is a house.")
-time.sleep(2)
-print("To your right is a dark cave.")
-time.sleep(2)
-print("In your hand you hold your trusty (but not very effective) dagger.")
-time.sleep(2)
-print(" ")
-time.sleep(2)
-print("Enter 1 to knock on the door of the house.")
-time.sleep(2)
-print("Enter 2 to peer into the cave.")
-time.sleep(2)
-print("What would you like to do?")
-time.sleep(2)
-print("(Please enter 1 or 2.)")
-time.sleep(2)
 
+def print_message(message):
+    print(message)
+    #time.sleep(2)
+
+print_message("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
+print_message("Rumor has it that a gorgon is somewhere around here, and has been terrifying the nearby village.")
+print_message("In front of you is a house.")
+print_message("To your right is a dark cave.")
+print_message("In your hand you hold your trusty (but not very effective) dagger.")
+print_message("Enter 1 to knock on the door of the house.")
+print_message("Enter 2 to peer into the cave.")
+print_message("What would you like to do?")
+
+
+response = input("please say\n"
+                "1" "or" "2\n")
+while True:
+    if response == "1":
+        print("you are e")
+        break
+    elif response == "2":
+        print("here")
+        break
+    else:
+        print("sorry i don't understand")
+        break
 
